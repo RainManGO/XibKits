26aaf54126628602b3ca8b1c1769761a0dc2a6c8
zhangyu <776210576@qq.com>
2019-09-23 16:34:39 +0800

    xibTextField

---
+++ Example/Pods/Target Support Files/Pods-XibKits_Tests/Pods-XibKits_Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_XibKits_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XibKits_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/XibKits/XibKits-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XibKits : NSObject
+@end
+@implementation PodsDummy_XibKits
+@end
